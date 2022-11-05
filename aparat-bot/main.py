8bafc06f5523233c8e8444745bdc333582e438cc
Arya Kavian <39616643+aryakvn@users.noreply.github.com>
2022-11-05 22:17:57 +0330

    Fixed Input and Button Selectors
    Fixed credential input and login button css selectors to work with the latest instagram login form.

--- aparat-bot/main.py
+++ aparat-bot/main.py
@@ -20,18 +20,18 @@ driver.get('https://www.instagram.com/accounts/login/')
 
 time.sleep(5)
 
-enter_username = driver.find_element_by_css_selector('#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(2) > div > label > input')
+enter_username = driver.find_element_by_css_selector('#loginForm > div > div:nth-child(1) > div > label > input')
 enter_username.send_keys(username)
 
 
 # 
 
-enter_password = driver.find_element_by_css_selector('#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(3) > div > label > input')
+enter_password = driver.find_element_by_css_selector('#loginForm > div > div:nth-child(2) > div > label > input')
 enter_password.send_keys(password)
 
 # 
 time.sleep(2)
-click_login = driver.find_element_by_css_selector('#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div.Igw0E.IwRSH.eGOV_._4EzTm.bkEs3.CovQj.jKUp7.DhRcB > button')
+click_login = driver.find_element_by_css_selector('#loginForm > div > div:nth-of-type(3) > button[type="submit"]')
 click_login.click()
 
 time.sleep(5)
